15ca3d633ccceee984d069c30f8920db6372cc0f
Siddharth Kumar <144512053+Sid541@users.noreply.github.com>
2025-12-12 20:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+let name = document.getElementById("name");
+let age = document.getElementById("age")
+let submit = document.getElementById("btn")
+
+submit.addEventListener("click",(e)=>{
+	console.log(name)
+    if(name.value !== "" && age.value !== ""){
+        ageCheck(parseInt(age.value));
+    }
+    else{
+        alert("Please enter valid details.")
+    }
+})
+
+function ageCheck(age){
+    let promise = new Promise((res,rej)=>{
+        setTimeout(()=>{
+            if(age > 18){
+                alert("Welcome, . You can vote.");
+                res("you can vote");
+            }else{
+                alert("Oh sorry . You aren't old enough.");
+                rej("Oh sorry . You aren't old enough.")
+            }
+        },4000)
+    })
+}
